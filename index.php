4c578257d80ce5b8c39a328b7a95600fb5168087
guyllaume beaudry <guyllaumebeaudry@gmail.com>
2024-09-04 16:44:05 -0400

    Affichage de base avec navbar

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+$strTitreApplication = 'Projet PHP';
+$strNomFichierCSS = 'style/style.css';
+require_once 'librairies-communes-2018-mm-jj.php';
+require_once 'en-tete.php';
+
+?>
+    <div class="contenu">
+
+    </div>
+
+<?php
+require_once 'pied-page.php';
+
+?>
+    
