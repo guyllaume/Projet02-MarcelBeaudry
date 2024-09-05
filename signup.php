bb91a419a5ce013d0d58314f1c02f0964e70861d
guyllaume beaudry <guyllaumebeaudry@gmail.com>
2024-09-04 21:21:36 -0400

    Login V0.2 SignUp V0.3 ForgotPassword V0.1
    Ajout de validation coté client avec regex

--- signup.php
+++ signup.php
@@ -15,26 +15,77 @@ require_once 'en-tete.php';
                     <div class="form-group">
                         <div class="form-subgroup">
                             <label for="email">Email</label>
-                            <input type="email" class="form-control" id="email" name="email">
+                            <div class="input-group">
+                                <input type="email" class="form-control" id="email" name="email">
+                                <span class="error" id="errorEmail">&nbsp;</span>
+                            </div>
                         </div>
                         <div class="form-subgroup">
-                        <label for="email">Confirmation Email</label>
-                            <input type="email" class="form-control" id="email" name="email">
+                            <label for="email">Confirmation Email</label>
+                            <div class="input-group">
+                                <input type="email" class="form-control" id="confirmationEmail" name="email">
+                                <span class="error" id="errorConfirmationEmail">&nbsp;</span>
                             </div>
+                        </div>
                         <div class="form-subgroup">
                             <label for="password">Mot de passe</label>
-                            <input type="password" class="form-control" id="password" name="password">
+                            <div class="input-group">
+                                <input type="password" class="form-control" id="password" name="password">
+                                <span class="error" id="errorPassword">&nbsp;</span>
+                            </div>
                         </div>
                         <div class="form-subgroup">
                             <label for="password">Confirmation Mot de passe</label>
-                            <input type="password" class="form-control" id="password" name="password">
+                            <div class="input-group">
+                                <input type="password" class="form-control" id="confirmationPassword" name="password">
+                                <span class="error" id="errorConfirmationPassword">&nbsp;</span>
+                            </div>
                         </div>
-                    <button type="submit" class="btn btn-primary">Submit</button>
+                    <button type="button" id="btnSubmit" class="btn btn-primary">Submit</button>
                     </div>
                 </form>
             </div>
         </div>
     </div>
+
+    <script>
+        document.getElementById('btnSubmit').addEventListener('click', function() {
+            const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+            const passwordRegex = /^[a-z0-9]{5,15}$/;
+            const email = document.getElementById("email").value;
+            const confirmationEmail = document.getElementById("confirmationEmail").value;
+            const password = document.getElementById("password").value;
+            const confirmationPassword = document.getElementById("confirmationPassword").value;
+            let informationIsCorrect = true;
+            if(!emailRegex.test(email)) {
+                document.getElementById("errorEmail").innerHTML = "L'email n'est pas valide";
+                informationIsCorrect = false;
+            }else{
+                document.getElementById("errorEmail").innerHTML = "&nbsp;";
+            }
+            if(!passwordRegex.test(password)) {
+                document.getElementById("errorPassword").innerHTML = "Le mot de passe doit contenir entre 5 et 15 caractères";
+                informationIsCorrect = false;
+            }else{
+                document.getElementById("errorPassword").innerHTML = "&nbsp;";
+            }
+            if(email != confirmationEmail) {
+                document.getElementById("errorConfirmationEmail").innerHTML = "Les emails ne sont pas identiques";
+                informationIsCorrect = false;
+            }else{
+                document.getElementById("errorConfirmationEmail").innerHTML = "&nbsp;";
+            }
+            if(password != confirmationPassword) {
+                document.getElementById("errorConfirmationPassword").innerHTML = "Les mots de passe ne sont pas identiques";
+                informationIsCorrect = false;
+            }else{
+                document.getElementById("errorConfirmationPassword").innerHTML = "&nbsp;";
+            }
+            if(informationIsCorrect) {
+                this.form.submit();
+            }
+        })
+    </script>
 
 <?php
 require_once 'pied-page.php';
